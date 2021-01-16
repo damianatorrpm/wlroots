bfe901d43998277303c883551c089120815ad2be
Damian Ivanov <damianatorrpm@gmail.com>
2021-01-16 22:12:33 +0200

    test

--- xwayland/xwm.c
+++ xwayland/xwm.c
@@ -751,7 +751,7 @@ static void read_surface_property(struct wlr_xwm *xwm,
 	} else if (property == xwm->atoms[WM_WINDOW_ROLE]) {
 		read_surface_role(xwm, xsurface, reply);
 	} else if (property == xwm->atoms[NET_WM_USER_TIME]) {
-		read_surface_role(xwm, xsurface, reply);
+		wlr_log(WLR_DEBUG, "Handle NET_WM_USER_TIME");
 	} else {
 		char *prop_name = xwm_get_atom_name(xwm, property);
 		wlr_log(WLR_DEBUG, "unhandled X11 property %" PRIu32 " (%s) for window %" PRIu32,
